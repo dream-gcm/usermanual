c3cbc5d04c45e362d6cffa2f6a787ebc6c075534
Stephanie Leroux <stephanieleroux@users.noreply.github.com>
2023-09-29 10:07:52 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -76,23 +76,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for PDF output -------------------------------------------------
-latex_engine = 'xelatex'
 latex_elements = {
-    'fontpkg': r'''
-\setmainfont{DejaVu Serif}
-\setsansfont{DejaVu Sans}
-\setmonofont{DejaVu Sans Mono}
-''',
-    'preamble': r'''
-\usepackage[titles]{tocloft}
-\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
-\setlength{\cftchapnumwidth}{0.75cm}
-\setlength{\cftsecindent}{\cftchapnumwidth}
-\setlength{\cftsecnumwidth}{1.25cm}
-''',
-    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
-    'printindex': r'\footnotesize\raggedright\printindex',
     'figure_align':'H'
 }
-latex_show_urls = 'footnote'
-
