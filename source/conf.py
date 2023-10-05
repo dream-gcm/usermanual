1a4bc34c01f37159e08fe3c362b227f08f92637d
Stephanie Leroux <stephanieleroux@users.noreply.github.com>
2023-10-05 16:11:02 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -122,7 +122,7 @@ latex_elements = {
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = '/img/cover.png'
+latex_logo = './img/cover.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
